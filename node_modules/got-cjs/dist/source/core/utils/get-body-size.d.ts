0c9666fe2c672b40d93b8170182a94aff362ba3a
bibinwithab <bibingraceson@gmail.com>
2023-09-07 18:57:19 +0530

    opened in work env

--- node_modules/got-cjs/dist/source/core/utils/get-body-size.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="node" />
-import type { ClientRequestArgs } from 'http';
-export default function getBodySize(body: unknown, headers: ClientRequestArgs['headers']): Promise<number | undefined>;
--- node_modules/got-cjs/dist/source/core/utils/is-client-request.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function isClientRequest(clientRequest) {
-    return clientRequest.writable && !clientRequest.writableEnded;
-}
-exports.default = isClientRequest;
