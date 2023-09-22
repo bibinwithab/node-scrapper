8fc215fa8c3f9d14e8faabdd434798fa93279a76
Bibin Graceson J <118437373+bibinwithab@users.noreply.github.com>
2023-09-22 11:04:19 +0530

    model added

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const urlencodedparser = bodyParser.urlencoded({extended:false});
 app.set( 'view engine', 'ejs' );
 
 app.get('/', (req, res) => {
-    res.render('index',{ price:'' , name:'' });
+    res.render('index',{ price:'' , name:'', model:'' });
 });
 
 app.post('/send-url', urlencodedparser, async (req, res) => {
@@ -21,10 +21,11 @@ app.post('/send-url', urlencodedparser, async (req, res) => {
         const html = response.data;
         const $ = cherrio.load(html);
         const prices = $('div.our_price');
-        const names = $('h1.like-h3');
+        const names = $('h1[itemprop="name"]');
+        const models = $('span.model');
         console.log(names.text());
         console.log(prices.text());
-        res.render('index', { price: prices.text(), name:names.text() });
+        res.render('index', { price: prices.text(), name:names.text(), model: models.text() });
 
     })
     .catch(err => console.log(err))
